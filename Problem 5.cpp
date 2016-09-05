fee4272d7651bde2b44d61e561a9649da5af689e
Apara <ssapar@gmail.com>
2016-09-05 14:52:51 -0600

    Solution to Problem 5 added
    Runtime ~ 2.7 s

---
+++ Problem 5.cpp
@@ -0,0 +1,30 @@
+/* Problem 4: Finding Smallest Multiple
+ *
+ */
+
+#include <iostream>
+
+int evenlyDivides(int first, int last);
+
+int main(){
+
+    std::cout << evenlyDivides(1, 20) << std::endl;
+
+    return 0;
+}
+
+int evenlyDivides(int first, int last){
+    int num = last;
+    int counter = first;
+
+    while(counter <= last){
+        if (num % counter != 0){
+            num += num % counter;
+            counter = first;
+            continue;
+        }
+        counter++;
+    }
+
+    return num;
+}
