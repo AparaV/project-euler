3b8e1f0d6fc443e1fcef41ffa3c3acd673a9dac9
Apara <ssapar@gmail.com>
2016-12-23 15:58:16 -0700

    Add solution to problem 18

--- Problem 018/Problem_18.py
+++ Problem 018/Problem_18.py
@@ -1,24 +1,27 @@
 def readFile(filename):
     f = open(filename, "r")
-    return f.readlines()
+    array = []
+    for line in f:
+        line = line.strip("\n")
+        array.append(line.split(" "))
+    tree = []
+    for row in array:
+        tree.append(map(int, row))
+    return tree
 
 def calculateSum(filename):
     tree = readFile(filename)
-    sum = 0 
-    for i in range(0, len(tree)-1):
-        if i == 0:
-            j = 0
-            sum += tree[i][0]
-        else if tree[i+1][j] > tree[i+1][j+1]:
-            sum += tree[i+1][j]
-        else:
-            sum += tree[i+1][j+1]
-            j += 1
-    return sum
+    for i in range(len(tree) - 2, -1, -1):
+        for j in range(len(tree[i])):
+            c = tree[i][j] + tree[i + 1][j]
+            d = tree[i][j] + tree[i + 1][j + 1]
+            tree[i][j] = max(c, d)
+    return tree[0][0]
+
+
 
 def main():
-    print calculateSum("tree.txt")
+    print calculateSum("data.txt")
 
 if __name__ == "__main__":
     main()
-                
