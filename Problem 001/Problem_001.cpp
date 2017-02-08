0040827453c69ca01b5234213860e6b9edc0b986
Apara <ssapar@gmail.com>
2017-02-08 12:39:57 -0700

    Optimize problem 001

--- Problem 001/Problem_001.cpp
+++ Problem 001/Problem_001.cpp
@@ -1,22 +1,34 @@
-#include <iostream>
+/** Problem 001: Multiples of 3 and 5
+ ** Author: Aparajithan Venkateswaran
+ */
 
-int sum3and5Multiples(int upperLimit){
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int answer(){
     int sum = 0;
 
-    for(int i = 0; 3*i < upperLimit; ++i){
-        sum += 3 * i;
-    }
-    for(int i = 0; 5*i < upperLimit; ++i){
-        sum += 5*i;
-    }
-    for(int i = 0; 15*i < upperLimit; ++i){
-        sum -= 15*i;
-    }
+	int threes = 999 / 3;
+	sum += 3 * threes * (threes + 1) / 2;
+	int fives = 999 / 5;
+	sum += 5 * fives * (fives + 1) / 2;
+	int fifteens = 999 / 15;
+	sum -= 15 * fifteens * (fifteens + 1) / 2;
 
     return sum;
 }
 
 int main(){
-    std::cout << sum3and5Multiples(1000) << std::endl;
+
+	clock_t start = clock();
+	int solution = answer();
+	clock_t end = clock();
+	double time = (double)(end - start) / CLOCKS_PER_SEC * 1000.0;
+
+	cout << "Answer is " << solution << endl;
+	cout << "It took " << time << " milliseconds" << endl;
+
     return 0;
 }
